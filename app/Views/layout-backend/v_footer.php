fce012b1a8d820c3a13d11cf9e836a440861b20f
Egova <egovaflavia@gmail.com>
2020-09-04 16:47:54 +0700

    Issue #1 Fixed

--- app/Views/layout-backend/v_footer.php
+++ app/Views/layout-backend/v_footer.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; <?= date('Y'); ?> <a href="http://mediatamaweb.co.id">PAIWestSumatraTours</a>.</strong>
+    <strong>Copyright &copy; <?= date('Y'); ?> <a href="http://mediatamaweb.co.id">PASAIWestSumatraTours</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>egvflv</b> 1.0
